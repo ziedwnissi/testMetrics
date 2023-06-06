a2566b39966f00082b0b4acc99e22e70f05ffcf9
Zied <zied.ounissi-ext@sanofi.com>
2023-06-06 15:04:50 +0200

    add test

---
+++ test.spec.js
@@ -0,0 +1,12 @@
+
+
+describe('test', () => {
+
+  it('should be equal', () => {
+    expect(1).toEqual(1);
+  });
+
+  it('calls advancedFilterService', async () => {
+   expect(1).toBe(1);
+  });
+});
